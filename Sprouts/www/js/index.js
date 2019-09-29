0a3c65e11b343460fb07ee0c09b318fa834b2ead
terziev-viktor <viktor.terziev@acronis.com>
2019-09-29 17:32:09 +0300

    Started work on game steps flow

--- Sprouts/www/js/index.js
+++ Sprouts/www/js/index.js
@@ -17,7 +17,93 @@
  * under the License.
  */
 
+// the array in touchmove event args
 var ongoingTouches = [];
+
+// the circles drawn on the screen
+var circles = [];
+
+// width and height of the screen == the canvas
+var WIDTH, HEIGHT;
+
+// width of the lines we draw
+const LINE_WIDTH = 4;
+
+// the radius of circles we draw
+const CIRCLE_RADIUS = 8;
+
+// the c in a*a + b*b = c*c
+const alpha = CIRCLE_RADIUS * 2;
+
+// if the player is close enough to the other circle we will say that he touched it
+const epsilon = 3; 
+
+// a map of the canvas as a matrix of pixels and stuff
+var Matrix;
+
+// number of players whose turn is
+var player = 1;
+
+// Its time to make a new circle because a player linked 2 circles
+var shouldCreateNewCircle = false;
+
+// we are drawing because the player did not make a wrong move
+var drawing = true;
+
+var the_step;
+
+class Circle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    get X() {
+        return this.x;
+    }
+
+    get Y() {
+        return this.y;
+    }
+
+    collides(other) {
+        let a = Math.abs(this.x - other.X);
+        let b = Math.abs(this.y - other.Y);
+        return a*a + b*b <= alpha * alpha + epsilon;
+    }
+};
+
+class CANVASMATRIX {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+        this.arr = new Array(this.width * this.height);
+    }
+
+    get height() {
+        return this.height;
+    }
+
+    get width() {
+        return this.width;
+    }
+
+    at(row, col) {
+        if(row < this.height && col < this.width) {
+            return this.arr[row * this.width + col];
+        } else {
+            throw "Invalid row and/or col in at(row, col)";
+        }
+    }
+
+    set_at(row, col, some_value) {
+        if(row < this.height && col < this.width) {
+            this.arr[row * this.width + col] = some_value;
+        } else {
+            throw "Invalid row and/or col in set_at(row, col, some_value)";
+        }
+    }
+};
 
 function colorForTouch(touch) {
     var r = Math.random() * 255;
@@ -47,98 +133,170 @@ function ongoingTouchIndexById(idToFind) {
     return -1;    // not found
 };
 
+function getCollidingCircleIndex(some_circle) {
+    for(var i = 0; i < circles.length; ++i) {
+        if(some_circle.collides(circles[i])) {
+            return i;
+        }
+    }
 
+    return -1;
+};
+
+function drawCircle(ctx, x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, CIRCLE_RADIUS, 0, 2 * Math.PI, false);
+    ctx.stroke();
+}
+
+function drawCircle(circle) {
+    let el = document.getElementsByTagName("canvas")[0];
+    let ctx = el.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(circle.X, circle.Y, CIRCLE_RADIUS, 0, 2 * Math.PI, false);
+    ctx.stroke();
+}
+
+// object for syncronizing the game steps
+class Step {
+    constructor() {
+        this.current = 0;
+        this.number_of_steps = 4;
+    }
+
+    next() {
+        ++this.current;
+        this.current %= this.number_of_steps;
+    }
+
+    prev() {
+        --this.current;
+        if(this.current < 0) {
+            this.current = this.number_of_steps - 1;
+        }
+    }
+
+    equals(step_str) {
+        if(this.current === 0 && step_str === "link_begin") { // link a circle with another with a line
+            return true;
+        }
+
+        if(this.current === 1 && step_str === "link") { // draw a new circle on the last drawn line
+            return true;
+        }
+
+        if(this.current === 2 && step_str === "link_end") { // draw a new circle on the last drawn line
+            return true;
+        }
+
+        if(this.current === 3 && step_str === "draw_new_circle") { // draw a new circle on the last drawn line
+            return true;
+        }
+
+        return false;
+    }
+}
+
+function circleIsOnALine(circle) {
+    return true; // mock of the function for now
+}
 
 var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-
         var thecanvas = document.getElementsByTagName('canvas')[0];
-        thecanvas.height = window.screen.height;
-        thecanvas.width = window.screen.width;
+        WIDTH = window.screen.width;
+        HEIGHT = window.screen.height;
+        thecanvas.height = HEIGHT;
+        thecanvas.width = WIDTH;
+        the_step = new Step();
 
         thecanvas.addEventListener("touchstart", function (evt) { // Handle touch start
-            console.log('Handling touch start:')
             evt.preventDefault();
-            var el = document.getElementsByTagName("canvas")[0];
-            var ctx = el.getContext("2d");
-            var touches = evt.changedTouches;
 
-            ongoingTouches.push(copyTouch(touches[0]));
-            var color = colorForTouch(touches[0]);
-            ctx.beginPath();
-            ctx.arc(touches[0].pageX, touches[0].pageY, 8, 0, 2 * Math.PI, false);  // a circle at the start
-            ctx.strokeStyle = color;
-            ctx.stroke();
-        
-        }, false);
-
-        thecanvas.addEventListener("touchend", function (evt) { // Handle touch end
-            console.log('Handling touch end:')
-            evt.preventDefault();
-            var el = document.getElementsByTagName("canvas")[0];
-            var ctx = el.getContext("2d");
-            var touches = evt.changedTouches;
-
-            //var color = colorForTouch(touches[0]);
-            var idx = ongoingTouchIndexById(touches[0].identifier);
-
-            if (idx >= 0) {
-                ctx.beginPath();
-                ctx.arc(touches[idx].pageX, touches[idx].pageY, 8, 0, 2*Math.PI, true);
-                var color = colorForTouch(touches[0]);
-                ctx.strokeStyle = color;
-                ctx.stroke();
-                
-                ongoingTouches.splice(idx, 1);  // remove it; we're done
-            } else {
-                console.log("can't figure out which touch to end");
+            if(the_step.equals("draw_new_circle")) {   
+                let touches = evt.changedTouches;
+                ongoingTouches.push(copyTouch(touches[0])); 
+                let next_circle = new Circle(touches[0].pageX, touches[0].pageY);
+                if(circleIsOnALine(next_circle)) {
+                    drawCircle(next_circle);
+                    circles.push(next_circle);
+                    the_step.next();
+                }
+            } else if(the_step.equals("link_begin")) { 
+                let touches = evt.changedTouches;
+                ongoingTouches.push(copyTouch(touches[0]));
+                let current_circle = new Circle(touches[0].pageX, touches[0].pageY);
+                let colliding_cirlce = getCollidingCircleIndex(current_circle);
+                if(colliding_cirlce > -1) {
+                    the_step.next();
+                }
+            } else  {
+                // maybe we want more steps ?
             }
             
         }, false);
 
-        thecanvas.addEventListener("touchcancel", function (evt) { // Handle touch cancel
-            console.log('Handling touch cancel:')
+        thecanvas.addEventListener("touchend", function (evt) { // Handle touch end
             evt.preventDefault();
-            console.log("touchcancel.");
-            var touches = evt.changedTouches;
+            if(the_step.equals("link")) {
+                the_step.next(); // set to link_end, because in touchmove we never know when we are going to stop
+
+                let touches = evt.changedTouches;
+                let idx = ongoingTouchIndexById(touches[0].identifier);
+                if (idx >= 0) {
+                    let c  = new Circle(touches[idx].pageX, touches[idx].pageY);
+                    let i = getCollidingCircleIndex(c);
+                    if(i == -1) {
+                        alert("UNDO not implemented");
+                        the_step.next();
+                    } else {
+                        the_step.next(); // draw new circle
+                    }
+                    ongoingTouches.splice(idx, 1);  // remove it; we're done
+                } else {
+                    console.log("error: indx < 0");
+                }
+            }
+        }, false);
+
+        thecanvas.addEventListener("touchcancel", function (evt) { // Handle touch cancel
+            evt.preventDefault();
+            let touches = evt.changedTouches;
   
-            for (var i = 0; i < touches.length; i++) {
-                var idx = ongoingTouchIndexById(touches[i].identifier);
+            for (let i = 0; i < touches.length; i++) {
+                let idx = ongoingTouchIndexById(touches[i].identifier);
                 ongoingTouches.splice(idx, 1);  // just remove it, we're done
             }
         }, false);
 
         thecanvas.addEventListener("touchmove", function (evt) { // Handle touch move
-            console.log('Handling touch move:')
             evt.preventDefault();
-            var el = document.getElementsByTagName("canvas")[0];
-            var ctx = el.getContext("2d");
-            var touches = evt.changedTouches;
+            if(the_step.equals("link")) {
+                let el = document.getElementsByTagName("canvas")[0];
+                let ctx = el.getContext("2d");
+                let touches = evt.changedTouches;
 
-            for (var i = 0; i < touches.length; i++) {
-                var color = colorForTouch(touches[i]);
-                var idx = ongoingTouchIndexById(touches[i].identifier);
+                for (let i = 0; i < touches.length; i++) {
+                    let color = colorForTouch(touches[i]);
+                    let idx = ongoingTouchIndexById(touches[i].identifier);
 
-                if (idx >= 0) {
-                console.log("continuing touch "+idx);
-                ctx.beginPath();
-                console.log("ctx.moveTo(" + ongoingTouches[idx].pageX + ", " + ongoingTouches[idx].pageY + ");");
-                ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-                console.log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
-                ctx.lineTo(touches[i].pageX, touches[i].pageY);
-                ctx.lineWidth = 4;
-                ctx.strokeStyle = color;
-                ctx.stroke();
+                    if (idx >= 0) {
+                        ctx.beginPath();
+                        ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+                        ctx.lineTo(touches[i].pageX, touches[i].pageY);
+                        ctx.lineWidth = LINE_WIDTH;
+                        ctx.strokeStyle = color;
+                        ctx.stroke();
 
-                ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-                console.log(".");
-                } else {
-                    console.log("can't figure out which touch to continue");
+                        ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+                    }
                 }
             }
         }, false);
+
+        Matrix = new CANVASMATRIX(WIDTH, HEIGHT);
     },
     // deviceready Event Handler
     //
@@ -150,8 +308,13 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function() {
-        console.log('Device is ready');
-        
+        let circle1 = new Circle(WIDTH / 2 + 5 * CIRCLE_RADIUS, HEIGHT / 2), circle2 = new Circle(WIDTH / 2 - 5 * CIRCLE_RADIUS, HEIGHT / 2);
+        let el = document.getElementsByTagName("canvas")[0];
+        let ctx = el.getContext("2d");
+        circles.push(circle1);
+        circles.push(circle2);
+        drawCircle(circle1);
+        drawCircle(circle2);
     }
 };
 
